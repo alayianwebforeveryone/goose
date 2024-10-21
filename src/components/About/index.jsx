7889c26113b883388d6b1222e0b1f57a6217180e
H-Anjum8 <huma.anjum88@gmail.com>
2024-10-21 20:22:07 +0500

    chnage in about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const About = () => {
   return (
     <div>
-      <h1 className="text-4xl  font-bold text-center text-[white]">About</h1>
+      <h1 className="text-4xl  font-bold text-center text-[black]">This is about page</h1>
       <Card/>
     </div>
   );
